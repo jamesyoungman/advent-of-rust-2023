5fe45c1a71f008d77afb235cf7f017398599cbe3
James Youngman <james@youngman.org>
2023-12-03 07:42:29 +0000

    day 03: solved part 2.

--- src/bin/day03/main.rs
+++ src/bin/day03/main.rs
@@ -1,10 +1,29 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::str;
 
 use lib::grid::Position;
 
-fn is_symbol(ch: &char) -> bool {
-    *ch != '.' && !ch.is_ascii_digit()
+#[derive(Eq, PartialEq, Clone, Copy)]
+enum Symbol {
+    Gear(Position),
+    Other,
+}
+
+impl Symbol {
+    fn is_gear(&self) -> bool {
+        matches!(self, Symbol::Gear(_))
+    }
+}
+
+fn symbol_type(ch: char, pos: Position) -> Option<Symbol> {
+    if ch == '*' {
+        Some(Symbol::Gear(pos))
+    } else if ch == '.' || ch.is_ascii_digit() {
+        None
+    } else {
+        Some(Symbol::Other)
+    }
 }
 
 fn parse_input(input: &str) -> HashMap<Position, char> {
@@ -23,7 +42,7 @@ fn parse_input(input: &str) -> HashMap<Position, char> {
     result
 }
 
-fn has_symbol_neighbour(p: &Position, schematic: &HashMap<Position, char>) -> bool {
+fn symbol_neighbour(p: &Position, schematic: &HashMap<Position, char>) -> Option<Symbol> {
     for dy in [-1, 0, 1] {
         for dx in [-1, 0, 1] {
             if dx == 0 && dy == 0 {
@@ -34,26 +53,31 @@ fn has_symbol_neighbour(p: &Position, schematic: &HashMap<Position, char>) -> bo
                 y: p.y + dy,
             };
             if let Some(ch) = schematic.get(&neighbour) {
-                if is_symbol(ch) {
-                    return true;
+                let symtype = symbol_type(*ch, neighbour);
+                if symtype.is_some() {
+                    return symtype;
                 }
             }
         }
     }
-    false
+    None
 }
 
-fn extract_part_numbers(schematic: &HashMap<Position, char>) -> Vec<i64> {
+fn extract_part_numbers(
+    schematic: &HashMap<Position, char>,
+) -> (Vec<i64>, HashMap<Position, HashSet<usize>>) {
     let mut result = Vec::new();
+    let mut gears: HashMap<Position, HashSet<usize>> = HashMap::new();
     let mut current_num: Option<i64> = None;
-    let mut is_part_num: bool = false;
+    let mut associated_part: Option<Symbol> = None;
+
     for y in 0.. {
         for x in 0.. {
             let p = Position { x, y };
             match schematic.get(&p) {
                 None => {
                     if x == 0 {
-                        return result;
+                        return (result, gears);
                     } else {
                         break;
                     }
@@ -61,30 +85,61 @@ fn extract_part_numbers(schematic: &HashMap<Position, char>) -> Vec<i64> {
                 Some(ch) => {
                     if let Some(digit_value) = ch.to_digit(10) {
                         current_num = Some(current_num.unwrap_or(0) * 10 + i64::from(digit_value));
-                        if has_symbol_neighbour(&p, schematic) {
-                            is_part_num = true;
+                        let neighbour = symbol_neighbour(&p, schematic);
+                        match &neighbour {
+                            Some(Symbol::Gear(_)) => {
+                                associated_part = neighbour;
+                            }
+                            Some(Symbol::Other)
+                                if !associated_part.map(|sym| sym.is_gear()).unwrap_or(false) =>
+                            {
+                                associated_part = Some(Symbol::Other);
+                            }
+                            _ => (),
                         }
                     } else if let Some(n) = current_num {
-                        if is_part_num {
-                            result.push(n);
+                        match associated_part {
+                            Some(Symbol::Other) => {
+                                result.push(n);
+                            }
+                            Some(Symbol::Gear(gear_location)) => {
+                                let part_num_index = result.len();
+                                gears
+                                    .entry(gear_location)
+                                    .and_modify(|partnum_indices| {
+                                        partnum_indices.insert(part_num_index);
+                                    })
+                                    .or_insert({
+                                        let mut h = HashSet::new();
+                                        h.insert(part_num_index);
+                                        h
+                                    });
+                                result.push(n);
+                            }
+                            None => (),
                         }
                         current_num = None;
-                        is_part_num = false;
+                        associated_part = None;
                     }
                 }
             }
         }
     }
-    result
+    let gears = gears
+        .into_iter()
+        .filter(|(_, indices)| indices.len() > 1)
+        .collect();
+    (result, gears)
 }
 
 fn part1(schematic: &HashMap<Position, char>) -> i64 {
-    extract_part_numbers(schematic).iter().sum()
+    let (part_numbers, _) = extract_part_numbers(schematic);
+    part_numbers.iter().sum()
 }
 
-#[test]
-fn test_part1() {
-    let example = concat!(
+#[cfg(test)]
+fn get_example() -> String {
+    concat!(
         "467..114..\n",
         "...*......\n",
         "..35..633.\n",
@@ -95,8 +150,34 @@ fn test_part1() {
         "......755.\n",
         "...$.*....\n",
         ".664.598..\n",
-    );
-    assert_eq!(part1(&parse_input(example)), 4361);
+    )
+    .to_string()
+}
+
+#[test]
+fn test_part1() {
+    let example = get_example();
+    assert_eq!(part1(&parse_input(&example)), 4361);
+}
+
+fn part2(schematic: &HashMap<Position, char>) -> i64 {
+    let (part_numbers, gear_locations) = extract_part_numbers(schematic);
+    gear_locations
+        .values()
+        .filter(|partnum_indices| partnum_indices.len() > 1)
+        .map(|partnum_indices| {
+            partnum_indices
+                .iter()
+                .map(|index: &usize| part_numbers[*index])
+                .product::<i64>()
+        })
+        .sum()
+}
+
+#[test]
+fn test_part2() {
+    let example = get_example();
+    assert_eq!(part2(&parse_input(&example)), 467835);
 }
 
 fn get_input() -> HashMap<Position, char> {
@@ -106,4 +187,5 @@ fn get_input() -> HashMap<Position, char> {
 
 fn main() {
     println!("day 03 part 1: {}", part1(&get_input()));
+    println!("day 03 part 2: {}", part2(&get_input()));
 }
