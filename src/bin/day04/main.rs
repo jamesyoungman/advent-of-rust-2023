828cecb7fcdad7a084b672a43c0414de25fbc0e3
James Youngman <james@youngman.org>
2023-12-03 22:02:38 +0000

    Prepare for day 4.

---
+++ src/bin/day04/main.rs
@@ -0,0 +1,14 @@
+use std::str;
+
+fn part1(_s: &str) -> u64 {
+    todo!()
+}
+
+fn get_input() -> String {
+    // str::from_utf8(include_bytes!("input.txt")).unwrap().to_string()
+    todo!()
+}
+
+fn main() {
+    println!("day 04 part 1: {}", part1(&get_input()));
+}
