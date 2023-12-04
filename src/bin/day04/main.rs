afdcd678b634098bb5657db7b5bf4f9905a7837a
James Youngman <james@youngman.org>
2023-12-04 06:06:47 +0000

    day 04: solved part 2.

--- src/bin/day04/main.rs
+++ src/bin/day04/main.rs
@@ -11,48 +11,56 @@ struct Card {
 }
 
 impl Card {
-    fn score(&self) -> u32 {
+    fn count_wins(&self) -> usize {
         self.have
             .iter()
             .filter(|have| self.winners.contains(have))
-            .fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 })
+            .count()
+    }
+
+    fn part1_score(&self) -> u32 {
+        (1..=self.count_wins()).fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 })
     }
 }
 
 #[test]
-fn test_score_0() {
+fn test_part1_score_0() {
     let c0 = Card {
         have: vec![1].into_iter().collect(),
         winners: vec![2].into_iter().collect(),
     };
-    assert_eq!(c0.score(), 0);
+    assert_eq!(c0.count_wins(), 0);
+    assert_eq!(c0.part1_score(), 0);
 }
 
 #[test]
-fn test_score_1() {
+fn test_part1_score_1() {
     let c1 = Card {
         have: vec![1].into_iter().collect(),
         winners: vec![1].into_iter().collect(),
     };
-    assert_eq!(c1.score(), 1);
+    assert_eq!(c1.count_wins(), 1);
+    assert_eq!(c1.part1_score(), 1);
 }
 
 #[test]
-fn test_score_2() {
+fn test_part1_score_2() {
     let c2 = Card {
         have: vec![6, 7, 9].into_iter().collect(),
         winners: vec![6, 7, 10].into_iter().collect(),
     };
-    assert_eq!(c2.score(), 2);
+    assert_eq!(c2.count_wins(), 2);
+    assert_eq!(c2.part1_score(), 2);
 }
 
 #[test]
-fn test_score_3() {
+fn test_part1_score_3() {
     let c3 = Card {
         have: vec![6, 7, 9].into_iter().collect(),
         winners: vec![6, 7, 9].into_iter().collect(),
     };
-    assert_eq!(c3.score(), 4);
+    assert_eq!(c3.count_wins(), 3);
+    assert_eq!(c3.part1_score(), 4);
 }
 
 fn parse_number_list(s: &str) -> Result<HashSet<i32>, Fail> {
@@ -89,7 +97,7 @@ fn parse_input(s: &str) -> Result<Vec<Card>, Fail> {
 }
 
 fn part1(cards: &[Card]) -> u32 {
-    cards.iter().map(|card| card.score()).sum()
+    cards.iter().map(|card| card.part1_score()).sum()
 }
 
 #[test]
@@ -106,6 +114,40 @@ fn test_part1() {
     assert_eq!(part1(&cards), 13);
 }
 
+fn part2_holding(cards: &[Card]) -> Vec<usize> {
+    fn won(mut holding: Vec<usize>, (card_num, wins): (usize, usize)) -> Vec<usize> {
+        //eprintln!("card {} has {wins} wins", card_num + 1);
+        for card in (card_num + 1)..(card_num + 1 + wins) {
+            holding[card] += holding[card_num];
+        }
+        holding
+    }
+    let holding: Vec<usize> = cards.iter().map(|_card| 1).collect();
+    cards
+        .iter()
+        .map(|card| card.count_wins())
+        .enumerate()
+        .fold(holding, won)
+}
+
+fn part2(cards: &[Card]) -> usize {
+    part2_holding(cards).iter().sum()
+}
+
+#[test]
+fn test_part2() {
+    let example = concat!(
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\n",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\n",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\n",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\n",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\n",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11\n",
+    );
+    let cards = parse_input(example).expect("example should be valid");
+    assert_eq!(part2(&cards), 30);
+}
+
 fn get_input() -> Vec<Card> {
     let input = str::from_utf8(include_bytes!("input.txt")).unwrap();
     parse_input(input).expect("input should be valid")
@@ -113,4 +155,5 @@ fn get_input() -> Vec<Card> {
 
 fn main() {
     println!("day 04 part 1: {}", part1(&get_input()));
+    println!("day 04 part 2: {}", part2(&get_input()));
 }
