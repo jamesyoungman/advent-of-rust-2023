db6f7362f9b55256cb49c88ab79aa5ace3d4ea5f
James Youngman <james@youngman.org>
2023-12-09 07:30:14 +0000

    day 09: solved part 2.

--- src/bin/day09/main.rs
+++ src/bin/day09/main.rs
@@ -47,7 +47,7 @@ fn test_compute_successive_diffs() {
     );
 }
 
-fn extrapolate(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+fn extrapolate_right(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut endval = 0_i32;
     let mut result = Vec::with_capacity(input.len());
     for mut v in input.into_iter().rev() {
@@ -59,9 +59,9 @@ fn extrapolate(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 }
 
 #[test]
-fn test_extrapolate() {
+fn test_extrapolate_right() {
     assert_eq!(
-        extrapolate(vec![
+        extrapolate_right(vec![
             vec![10, 13, 16, 21, 30, 45],
             vec![3, 3, 5, 9, 15],
             vec![0, 2, 4, 6],
@@ -78,8 +78,39 @@ fn test_extrapolate() {
     );
 }
 
+fn extrapolate_left(input: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut endval = 0_i32; // value of left end
+    let mut result = Vec::with_capacity(input.len());
+    for mut v in input.into_iter().rev() {
+        endval = *v.first().unwrap() - endval;
+        v.insert(0, endval);
+        result.push(v);
+    }
+    result.into_iter().rev().collect()
+}
+
+#[test]
+fn test_extrapolate_left() {
+    assert_eq!(
+        extrapolate_left(vec![
+            vec![10, 13, 16, 21, 30, 45],
+            vec![3, 3, 5, 9, 15],
+            vec![0, 2, 4, 6],
+            vec![2, 2, 2],
+            vec![0, 0]
+        ],),
+        vec![
+            vec![5, 10, 13, 16, 21, 30, 45],
+            vec![5, 3, 3, 5, 9, 15],
+            vec![-2, 0, 2, 4, 6],
+            vec![2, 2, 2, 2],
+            vec![0, 0, 0]
+        ]
+    );
+}
+
 fn predict_next_value(v: Vec<i32>) -> i32 {
-    *extrapolate(compute_successive_diffs(v))
+    *extrapolate_right(compute_successive_diffs(v))
         .first()
         .unwrap()
         .last()
@@ -91,8 +122,22 @@ fn test_predict_next_value() {
     assert_eq!(predict_next_value(vec![10, 13, 16, 21, 30, 45]), 68);
 }
 
+fn predict_prior_value(v: Vec<i32>) -> i32 {
+    *extrapolate_left(compute_successive_diffs(v))
+        .first()
+        .unwrap()
+        .first()
+        .expect("input should not be empty")
+}
+
+#[test]
+fn test_predict_prior_value() {
+    assert_eq!(predict_prior_value(vec![10, 13, 16, 21, 30, 45]), 5);
+    assert_eq!(predict_prior_value(vec![0, 3, 6, 9, 12, 15]), -3);
+}
+
 fn part1(vv: Vec<Vec<i32>>) -> i32 {
-    vv.into_iter().map(|v| predict_next_value(v)).sum()
+    vv.into_iter().map(predict_next_value).sum()
 }
 
 #[test]
@@ -104,6 +149,22 @@ fn test_part1() {
             vec![10, 13, 16, 21, 30, 45]
         ]),
         114
+    );
+}
+
+fn part2(vv: Vec<Vec<i32>>) -> i32 {
+    vv.into_iter().map(predict_prior_value).sum()
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(
+        part2(vec![
+            vec![0, 3, 6, 9, 12, 15],
+            vec![1, 3, 6, 10, 15, 21],
+            vec![10, 13, 16, 21, 30, 45]
+        ]),
+        2
     );
 }
 
@@ -123,9 +184,7 @@ fn test_number_seq() {
 
 fn main() {
     let input = str::from_utf8(include_bytes!("input.txt")).unwrap();
-    let parsed_input: Vec<Vec<i32>> = input
-        .split_terminator('\n')
-        .map(|line| number_seq(line))
-        .collect();
-    println!("day 09 part 1: {}", part1(parsed_input));
+    let parsed_input: Vec<Vec<i32>> = input.split_terminator('\n').map(number_seq).collect();
+    println!("day 09 part 1: {}", part1(parsed_input.clone()));
+    println!("day 09 part 2: {}", part2(parsed_input));
 }
